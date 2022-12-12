f3536bc540104283ccfcb2118c75433c4b339803
cheo0 <eliseo_0@ciencias.unam.mx>
2022-12-11 21:37:30 -0600

    Estilos simples añadidos.

--- focokrispis/focokrispis/settings.py
+++ focokrispis/focokrispis/settings.py
@@ -39,8 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    "crispy_bootstrap5",
     'proposal',
+    'bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
